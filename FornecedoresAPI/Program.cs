c9f8ad3a82cb3052d9cedb2af0e598bd54710203
André Luiz Flôres <102196185+andreflroes@users.noreply.github.com>
2024-09-02 19:57:02 -0300

    feat: Remoção de comentários.

--- FornecedoresAPI/Program.cs
+++ FornecedoresAPI/Program.cs
@@ -15,13 +15,11 @@ builder.Services.AddDbContext<FornecedoresContext>(options =>
     ));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
